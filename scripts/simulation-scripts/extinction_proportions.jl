1c053db24a95bdb4f5ac8e97838e86aca0582c6a
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2026-02-20 22:04:41 -0500

    save some of the realized webs for later study

--- scripts/simulation-scripts/extinction_proportions.jl
+++ scripts/simulation-scripts/extinction_proportions.jl
@@ -19,9 +19,19 @@ using JLD2
 
 @info "Dependencies Loaded"
 
-global_temp = nothing
+# probably 4-5 in practice
+const LARGE_CASCADE_CUTOFF = 2
+const SAVE_PROBABILITY = 0.01
 
-function process_solution(sol, g, primary_extinctions, secondary_extinctions)
+function process_solution(
+    sol,   
+    g, 
+    primary_extinctions, 
+    secondary_extinctions,
+    foodweb_number,
+    sequence_number,
+    output_dir
+)
 
     idxs = extinction_indices(sol, primary_extinctions)
     df = DataFrame()
@@ -29,33 +39,47 @@ function process_solution(sol, g, primary_extinctions, secondary_extinctions)
 
     richness_sol = richness(sol)
     fwm = sol.prob.f.sys
-    hg = fwm.hg
 
+    # Run for each cascade in the run.
     for (sp, i1, i2) in idxs
 
         t1 = sol.t[i1]
         t2 = sol.t[i2]
 
         net = realized_network(sol, t1)
-
-        if (length ∘ interactions)(net) > 20
-            global global_temp = net
-        end
-
         trial = secondary_extinctions_during_trial(secondary_extinctions, t1, t2)
 
+        # Save the state of the sims to disk if this is a large cascade, so we
+        # can study it in more depth later.
+        # All of the large ones. And a small random chance of saving not-large
+        # ones so that we have smaller cascades for comparison.
+        if ((richness_sol[i1-1] - richness_sol[i2-1]) > LARGE_CASCADE_CUTOFF) | (rand() < SAVE_PROBABILITY)
+
+            mkpath(output_dir * "/realized_webs")
+            jldsave(
+                    output_dir *
+                    "/realized_webs/foodweb_fw$(foodweb_number)_seq$(sequence_number)_sp$(sp).jld2",
+                    true; 
+                    fwm = fwm,
+                    u_pre = sol[i1-1],
+                    extinction_target = sp
+            )
+        end
+
         push!(df, (
+            foodweb_number = foodweb_number,
+            sequence_number = sequence_number,
             retcode = sol.retcode,
             g = g,
-            extinction_species = sp, 
-            vulnerability_primary = vulnerability(net, sp),
-            generality_primary = generality(net, sp),
+            extinction_target = sp, 
+            vulnerability_target = vulnerability(net, sp),
+            generality_target = generality(net, sp),
             richness_pre = richness_sol[i1-1],
             richness_post = richness_sol[i2-1],
             secondary_extinctions = length(trial),
-            timespan_of_cascade = cascade_timespan(secondary_extinctions, t1, t2),
+            cascade_timespan = cascade_timespan(secondary_extinctions, t1, t2),
             avg_extinction_time = mean_extinction_time(secondary_extinctions, t1, t2),
-            trophic_range = cascade_trophic_range(net, last.(trial)),
+            cascade_trophic_range = cascade_trophic_range(net, last.(trial)),
             maximum_trophic_level = maximum_trophic_level(net),
             t1 = t1,
             t2 = t2 
@@ -106,7 +130,8 @@ function simulations(;
             extra_transient_time = 1_000
         )
 
-        jldsave(output_dir * "/foodweb_$fwm_num.jld2", true; 
+        mkpath(output_dir * "/initial_webs")
+        jldsave(output_dir * "/initial_webs/foodweb_$fwm_num.jld2", true; 
             web = fwm.hg, traits = traits
         )
 
@@ -119,7 +144,9 @@ function simulations(;
             seq = (shuffle ∘ species)(fwm)
             times = collect((1_000.0:time_between_extinctions:(length(seq) * 1_000.0)))
 
-            sols = simulation_batch(fwm, prob, process_solution;
+            ps(x, y, z, w) = process_solution(x, y, z, w, fwm_num, seq_num, output_dir)
+
+            sols = simulation_batch(fwm, prob, ps;
                 extinction_times = times,
                 extinction_order = seq,
                 g1 = g1,
@@ -128,8 +155,6 @@ function simulations(;
             );
 
             data = vcat(sols...)
-            data[!, :foodweb_number] .= fwm_num
-            data[!, :sequence_number] .= seq_num 
 
             if (fwm_num == 1) & (seq_num == 1)
 
