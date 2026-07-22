4b63deaf72aaa46a0ab8e263fe2a910c9779f95d
Jake Harvey <jacob.harvey@mail.mcgill.ca>
2026-07-22 15:25:53 -0400

    progress

--- scripts/analysis-scripts/trophic-difference.jl
+++ scripts/analysis-scripts/trophic-difference.jl
@@ -10,13 +10,9 @@ using CategoricalArrays
 using QuantileRegressions
 using LsqFit
 
-# One extinction
 df1 = CSV.read("sim-output/one-extinction-2026-04-30/data.csv", DataFrame)
-# Two extinctions
 df2 = CSV.read("sim-output/two-extinctions-2026-04-30/data.csv", DataFrame)
-# Three extinctions
 df3 = CSV.read("sim-output/three-extinctions-2026-04-30/data.csv", DataFrame)
-# Four extinctions
 df4 = CSV.read("sim-output/four-extinctions-2026-04-30/data.csv", DataFrame)
 
 df = vcat(df1, df2, df3, df4)
@@ -31,11 +27,11 @@ transform!(df,
     => :trophic_difference
 );
 
-# -------------------------------------------------------- #
-# Trophic Difference Analyses:                             #
-# Are top-down or bottom-up effects implicated more often? #
-# Positive is top-down, negative is bottom-up              #
-# -------------------------------------------------------- #
+# --------------------------------------------------------- #
+# Trophic Difference Analyses:                              #
+# Are top-down or bottom-up effects implicated more or less # 
+# often at differing levels of adaptive foraging? No.       #
+# --------------------------------------------------------- #
 
 filt = copy(df)
 filter!(:trophic_difference => !ismissing, filt)
@@ -55,7 +51,7 @@ boxplot!(ax,
     width = 0.05,
 )
 
-save("figures/trophic_difference_v_g.png", f)
+save("figures/sup_trophic_difference_v_g.png", f)
 
 # --------------------------------------------------------- #
 # Are top-down and bottom-up effects implicated differently #
@@ -76,12 +72,26 @@ ax = Axis(f[1,1];
 scatter!(ax, 
     filt[:, :extinction_proportion], 
     filt[:, :trophic_difference];    
+    color = filt[:, :g]
 )
 
+# This isn't just using the standard GLM package cause this looked like a case 
+# where the data was clearly heteroscedastic and weighted least squares would
+# need to be used. Turns out to have not really made a noticeable difference,
+# but we'll keep it since that's the right thing to do!
 model(t, p) = p[1] .+ (p[2] .* t)
-fitOLS = curve_fit(model, filt[:, :extinction_proportion], filt[:, :trophic_difference], [1.0, 1.0])
+fitOLS = curve_fit(model, 
+    filt[:, :extinction_proportion], 
+    filt[:, :trophic_difference], 
+    [1.0, 1.0]
+)
 wt = 1 ./ fitOLS.resid.^2
-fitWLS = curve_fit(model, filt[:, :extinction_proportion], filt[:, :trophic_difference], wt, [0.0, 0.0])
+fitWLS = curve_fit(model, 
+    filt[:, :extinction_proportion], 
+    filt[:, :trophic_difference], 
+    wt, 
+    [0.0, 0.0]
+)
 
 ablines!(ax, fitWLS.param[1], fitWLS.param[2], color=:red, linewidth = 3)
 
